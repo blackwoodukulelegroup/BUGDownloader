3f262d9f7433666b7b0e122d21f6ddc17e6b2e38
BlackwoodUkeGroup <blackwoodukulelegroup@gmail.com>
2020-07-27 11:00:27 +0930

    fix

--- BUGDownloader.py
+++ BUGDownloader.py
@@ -4,15 +4,13 @@ import os
 import json
 import requests
 import re
-import platform
 
-if platform.system() == 'Windows':
-    dirSep = "\\"
-    localDir = "C:\\Repos\\BUGDownloader\\test"
-else:
-    dirSep = "/"
-    # localDir = "/home/pi/Ukulele/BUG Chord Charts"
-    localDir = "/home/pi/Scripts/BUGDownloader/test"
+localDir = "/home/pi/Ukulele/BUG Chord Charts"
+# localDir = "/home/pi/Scripts/BUGDownloader/test"
+
+requestHeaders = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}
+
+dirSep = "/"
 
 apiURL = "https://script.google.com/macros/s/AKfycbx-0s1grPv0Wj_wXZUDRggB7Eac_c4TGHkMQ1aNOcNv41eCeg/exec"
 
@@ -38,14 +36,21 @@ def GetWebResource(url, proxies, maxAttempts, timeout):
         print("- attempt {0}".format(attemptCounter + 1))
         try:
             if proxies is None:
-                response = requests.get(url, timeout=timeout, headers=requestHeaders)
+                response = requests.get(url, headers=requestHeaders, timeout=timeout)
             else:
-                response = requests.get(url, proxies=proxies, timeout=timeout, headers=requestHeaders)
+                response = requests.get(url, headers=requestHeaders, proxies=proxies, timeout=timeout)
             
-            response.raise_for_status()
+            if response.status_code == 200:
+                successFlag = 1
+            else:
+                print("Requests.Get failed: {0}".format(response.status_code))
+                attemptCounter += 1
+            
+            # response.raise_for_status()
 
-            successFlag = 1
-        except:
+            # successFlag = 1
+        except requests.exceptions.RequestException as e:
+            print("GetWebResource Error: {0}".format(e))
             attemptCounter +=1
 
     if successFlag == 1:
@@ -85,7 +90,6 @@ errorCount = 0
 
 try:
     apiResult = GetWebResource(apiURL, proxies, 5, 30)
-
     bugSongs = json.loads(apiResult.text)
 
     print("Found {0} songs to download".format(len(bugSongs)))
@@ -94,23 +98,29 @@ try:
     progress = {'errors': 0, 'existing': 0, 'downloaded': 0}
 
     for songKey in bugSongs:
-
-        print('\rErrors: {0}  Downloaded: {1}  Existing: {2} ... '.format(progress['errors'], progress['downloaded'], progress['existing']), end='')
-              
+        print("SongKeyLoop:  Key = {0}".format(songKey))
         song = bugSongs[songKey]
+        print("SongKeyLoop: Title = {0}  Artist = {1}".format(song['title'],song['artist']))
         fileNameBase = song['title'] + ' - ' + song['artist']
+        print("SongKeyLoop: fileNameBase = {0}".format(fileNameBase))
         songURLs = song['URL']
         for urlKey in songURLs:
-            fileName = (fileNameBase + ' - ' + urlKey + '.pdf').replace('/','_').replace('\\','_').replace('?','')
+            print("UrlKeyLoop: Key = {0}".format(urlKey))
+            fileName = (fileNameBase + ' - ' + urlKey + '.pdf').replace('/','_').replace('\\','_').replace('⁄','_')
+            print("UrlKeyLoop: FileName = {0}".format(fileName))
             fileURL = songURLs[urlKey]
-            if (re.search('drive|scorpex|ozbcoz', fileURL)):
+            if (re.search('scorpex|ozbcoz|drive', fileURL)):
                 if os.path.isfile(localDir + dirSep + fileName):
+                    existCount += 1
                     print("exists: " + fileName)
                 else:
-                    if DownloadFile(localDir + dirSep + fileName, fileURL, proxies, 1, 10):
+                    print("downloading: " + fileName)
+                    if DownloadFile(localDir + dirSep + fileName, fileURL, proxies, 1, 10, False):
                         print("downloaded: " + fileName)
+                        downloadCount += 1
                     else:
                         print("error:" + fileName)
+                        errorCount += 1
 
 except:
     PrintException()
@@ -118,5 +128,7 @@ except:
     # print(errType)
     # print(errValue)
     # print(errTraceback)
+
+print("Errors: {0}  Downloaded: {1}  Existing: {2}".format(errorCount, downloadCount, existCount))
 
 print("finished")
